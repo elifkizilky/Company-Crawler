4ef0d67a65048a77e3c3bc5ef1d4d524b6bec3ef
Elif Kizilkaya <69367633+elifkizilky@users.noreply.github.com>
2024-06-01 23:22:06 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 # config.py
-API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent" 
-API_KEY = "AIzaSyBQrwmkwlmvx4MUBi76j4XaQ_olb6ToaRA" 
+API_URL = "" 
+API_KEY = "" 
